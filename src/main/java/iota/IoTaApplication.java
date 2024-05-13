56e4c89be3470267d9022a5007eec0da9817f8ac
su-giana <giananews@gmail.com>
2024-05-13 09:45:31 +0900

    feat: define main application start point

---
+++ src/main/java/iota/IoTaApplication.java
@@ -0,0 +1,13 @@
+package iota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTaApplication {
+    public static void main(String[] args) {
+
+
+        SpringApplication.run(IoTaApplication.class, args);
+    }
+}
